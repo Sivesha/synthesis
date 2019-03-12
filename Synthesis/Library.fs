75b719d36c50d51334317aa211d5d8b912df20f2
sivesha <g16b0398@campus.ru.ac.za>
2019-03-12 09:04:59 +0200

    Test 11 completed

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -69,8 +69,21 @@ let isLeap y =
                     |false -> false
    
 
-let month _ =
-    failwith "Not implemented"
+let month mm = match mm with
+                |1-> ("January" , 31)
+                |2-> ("February", 28)
+                |3-> ("March",31)
+                |4-> ("April",30)
+                |5-> ("May",31)
+                |6-> ("June",30)
+                |7-> ("July",31)
+                |8 -> ("August",31)
+                |9-> ("September",30)
+                |10-> ("October", 31)
+                |11-> ("November",30)
+                |12->("December",31)
+                |_ -> failwith "Invalid number"
+  
 
 let toBinary _ =
     failwith "Not implemented"
